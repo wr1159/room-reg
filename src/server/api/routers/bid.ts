827c70b8e1697fd59d1a42ffb9c40f3280fd6817
joeng03 <yinjoeng2003@gmail.com>
2023-12-11 12:48:16 +0800

    feat: Setup DB

---
+++ src/server/api/routers/bid.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  bidRoom: publicProcedure
+    .input(z.object({ studentId: z.number(), roomId: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      ctx.db.room.update({
+        where: {
+          id: input.roomId,
+        },
+        data: {
+          occupantId: input.roomId,
+        },
+      });
+    }),
+
+  // hello: publicProcedure
+  //   .input(z.object({ text: z.string() }))
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `Hello ${input.text}`,
+  //     };
+  //   }),
+
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //       },
+  //     });
+  //   }),
+
+  // getLatest: publicProcedure.query(({ ctx }) => {
+  //   return ctx.db.post.findFirst({
+  //     orderBy: { createdAt: "desc" },
+  //   });
+  // }),
+});
