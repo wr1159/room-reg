703ba9cd11a97ddc8325bec69866aa477bc7fd76
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-12-12 04:20:07 +0800

    feat: Update landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,13 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from "~/components/ui/navigation-menu";
 
 const Navbar: React.FC = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="gap-x-2 p-4 text-lg text-primary">
+    <NavigationMenu className="bg-muted/30">
+      <NavigationMenuList className="w-screen items-center justify-start gap-x-2 p-4 text-lg text-primary">
         <NavigationMenuLink
           href="/"
           className="mr-4 block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
